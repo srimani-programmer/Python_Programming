2a143ca6570193f64db8b7198bbf855543917435
srimani-programmer <srimani.scholar@gmail.com>
2018-10-26 19:30:51 +0530

    Initial Commit.

---
+++ Basics/BasicOperations.py
@@ -0,0 +1,20 @@
+# Printing the message to the screen.
+
+print('Hello,World')
+
+# Simple Arithematic Operations
+
+print(2 + 5)    # Adition
+
+print(5 - 2)    # Subtraction
+
+print(5 * 2)    # Multiplication
+
+print(5 / 2)    # Division
+
+print(5 % 2)    # Modulus
+
+print(5 ** 2)   # Power of 2 numbers.
+
+print(5 // 2)   # Integer Division.
+
