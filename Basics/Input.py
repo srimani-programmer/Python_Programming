7d750f3bbd9856b284242957dd016f86785495e0
srimani-programmer <srimani.scholar@gmail.com>
2018-10-26 19:39:07 +0530

    Input Operations.

---
+++ Basics/Input.py
@@ -0,0 +1,11 @@
+# Taking the Input from the user
+
+name = input('Enter your name:')
+age = int(input('Enter your age:'))
+gender = input('Enter your gender:')
+
+# Printing the result.
+print('Name:'+name)
+print('Age:{}'.format(age))
+print('Gender:'+gender)
+
