2e25c7355ec717911e4a5d8048b3c1cab6e73488
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 17:42:15 +0530

    Python Lists

---
+++ Basics/Lists.py
@@ -0,0 +1,17 @@
+list1 = [10,20.434,True,2323,False,3+4j]
+
+print(type(list1))
+
+print(list1)
+
+print(list1[0])
+
+# To add an external element
+
+list1.append('Sri Manikanta')
+
+print(list1)
+
+list1[0] = 'aravind'
+
+print(list1)
