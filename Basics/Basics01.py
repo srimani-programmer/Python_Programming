5306a922f666e52419914fb5cdd859ede6cfcb61
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 17:08:02 +0530

    Initial Scrap

---
+++ Basics/Basics01.py
@@ -0,0 +1,11 @@
+# Working with data analysis in python
+
+from matplotlib import pyplot
+
+# The below are x and y axis lines
+
+pyplot.plot([1,2,3,4,5,6],[2,4,6,8,10,12],color = 'purple',label = "General")
+
+# this below function is to display the graph
+pyplot.legend()
+pyplot.show()
