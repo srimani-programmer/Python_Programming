8ab5a60704243259ad9b7627d78f0a4da48e994e
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 17:26:21 +0530

    Final Commit for If Statements.

---
+++ Conditional Statements/IfBlock.py
@@ -0,0 +1,7 @@
+# working with simple if blocks
+
+user_input1 = eval(input("Enter the first Number:"))
+user_input2 = eval(input("Enter the second number:"))
+
+if user_input1 + user_input2 >= 0:
+    print("You are something but not zero")
---
+++ Conditional Statements/IfBlock2.py
@@ -0,0 +1,7 @@
+# 02 Assignment
+
+user_input1 = eval(input("Enter the first value:"))
+user_input2 = eval(input("Enter the second value:"))
+
+if user_input1 + user_input2 < 0:
+    print("You are lessthan zero , No problem God will defenitely saves you...!")
