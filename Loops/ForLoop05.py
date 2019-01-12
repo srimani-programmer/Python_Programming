72e7dd47bcf1b8242159c968bde953b86d893490
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 16:24:26 +0530

    Final Commit.

---
+++ Loops/ForLoop05.py
@@ -0,0 +1,13 @@
+# Assignment05 ---> printing a string 'n' times
+import time
+import sys
+# inputString = input("Enter a String value:")
+# inputValue = int(input("Enter a value to iterate the string:"))
+
+for str in range(0,10000):
+    print("Hello world")
+
+totalTime = time.time()
+print(totalTime)
+sys.exit(0)
+
