43fe51a48199ab6218ec4fd439f4eec9283c002c
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 17:32:45 +0530

    Final Commit.

---
+++ Functions/KeywordParameters.py
@@ -0,0 +1,8 @@
+# working with Keyword Arguments
+
+def rectangle(length,breadth,height):
+    return length * breadth * height
+
+print(rectangle(height=12,breadth=13,length=11))
+
+# NOTE:- we should not pass positional variables after keyword variables
