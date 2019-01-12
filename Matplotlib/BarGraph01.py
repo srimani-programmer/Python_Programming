09c33f9efc5ef90eeddb509fe5794427d8eb97f1
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 16:32:34 +0530

    Final Commit.

---
+++ Matplotlib/BarGraph01.py
@@ -0,0 +1,11 @@
+# working with Bar Graph
+
+from matplotlib import pyplot
+pyplot.bar([35,33,22,30,26],[32,38,25,34,30],color = 'g',label = 'Marks')
+
+pyplot.legend()
+pyplot.xlabel("x-axis")
+pyplot.ylabel('Y-axis')
+
+pyplot.show()
+
