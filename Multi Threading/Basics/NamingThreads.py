4d48146c3715a71fb2fddd61b4eee2479dcc7a9c
srimani-programmer <srimani.scholar@gmail.com>
2019-02-23 10:56:33 +0530

    Concept added.

--- Multi Threading/Basics/NamingThreads.py
+++ Multi Threading/Basics/NamingThreads.py
@@ -11,3 +11,16 @@ print('My current working thread is: {}'.format(current_thread().name))
 current_thread().setName('SriMani')
 
 print('Now My thread name is: {}'.format(current_thread().getName()))
+
+'''
+To get name
+---------------
+current_thread().getName()
+current_thread.name
+
+To set name
+-----------------
+current_thread().setname('Name of the thread')
+current_thread().name = 'Name of the thread'
+
+'''
